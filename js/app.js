f0fb443f5fa9ac4633a139062722f61d324858e1
Victor Portugal <victorportugalnobrega@gmail.com>
2024-09-08 13:59:42 -0300

    Populando a base de dados e corrigindo pequenos erros

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function pesquisar(){
     let section = document.getElementById("resultados-pesquisa")
-    let campoPesquisa = document.getElementById("campo-pesquisa").valeu
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
 
     if(!campoPesquisa){
         // section.innerHTML = "<p>Nenhum resultado foi encontrado durante a sua busca. Por favor, digite o nome ou gênero de um anime ou o nome de um personagem.</p>"
@@ -12,21 +12,21 @@ function pesquisar(){
 
     let resultados = ""
     let titulo = ""
-    let descricao = ""
+    let sinopse = ""
     let tags = ""
 
     for(let dado of dados){
         titulo = dado.titulo.toLowerCase()
-        descricao = dado.descricao.toLowerCase()
+        sinopse = dado.sinopse.toLowerCase()
         tags = dado.tags.toLowerCase()
 
-        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+        if(titulo.includes(campoPesquisa) || sinopse.includes(campoPesquisa) || tags.includes(campoPesquisa)){
             resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
                 </h2>
-                <p class="descricao-meta">${dado.descricao}</p>
+                <p class="sinopse-meta">${dado.sinopse}</p>
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
             `
---
+++ js/dados.js
@@ -0,0 +1,37 @@
+let dados = [
+    {
+        "titulo": "Dragon Ball Super",
+        "sinopse": "Dragon Ball Super é a continuação oficial da saga Dragon Ball Z, apresentando novas aventuras do protagonista Goku e seus amigos após a derrota de Majin Boo. A série explora um universo muito mais vasto, apresentando novos personagens, vilões poderosos e transformações ainda mais surpreendentes. A trama principal se concentra em Goku, que, após alcançar a paz na Terra, busca novos desafios e poderes. Ele acaba encontrando seres divinos como Bills, o Deus da Destruição, e Whis, seu anjo guardião. A partir desse encontro, Goku é apresentado a um universo repleto de deuses, torneios intergalácticos e ameaças cósmicas.",
+        "genero": "Ação Fantasia Shonen",
+        "link": "https://www.crunchyroll.com/series/GR19V7816/dragon-ball-super",
+        "tags": "Goku Vegeta Gohan Beerus Whis Trunks Super Sayajin Deuses"
+    },
+    {
+        "titulo": "Jujutsu Kaisen",
+        "sinopse": "Yuji Itadori, um adolescente comum, vê sua vida mudar drasticamente quando ele ingere um dedo amaldiçoado, tornando-se um recipiente para o poderoso Sukuna, um dos Reis das Maldições. Agora, ele é forçado a entrar no mundo oculto dos feiticeiros jujutsu para aprender a controlar seus novos poderes e proteger aqueles que ama. A série acompanha Yuji e seus colegas de classe enquanto eles enfrentam ameaças sobrenaturais e desvendam os mistérios do mundo das maldições.",
+        "genero": "Ação Sobrenatural Shonen",
+        "link": "https://www.crunchyroll.com/series/GRDV0019R/jujutsu-kaisen",
+        "tags": "Yuji Itadori Sukuna Feitiçaria Maldições Gojo Satoru Megumi Fushiguro"
+    },
+    {
+        "titulo": "Naruto",
+        "sinopse": "Naruto Uzumaki é um jovem ninja da Vila da Folha que sonha em se tornar o Hokage, o líder da vila. Apesar de ser desprezado por seus companheiros por abrigar a Kyuubi, a Raposa de Nove Caudas, Naruto nunca desiste de seus sonhos. Acompanhe sua jornada enquanto ele treina para se tornar um ninja poderoso e faz amigos que o ajudarão a enfrentar os desafios que surgirão em seu caminho.",
+        "genero": "Ação Aventura Shonen Fantasia",
+        "link": "https://www.crunchyroll.com/series/GY9PJ5KWR/naruto",
+        "tags": "Naruto Uzumaki Sasuke Uchiha Sakura Haruno Kakashi Hatake Ninja Vila da Folha"
+    },
+    {
+        "titulo": "Shingeki no Kyojin",
+        "sinopse": "Em um mundo cercado por muralhas gigantes, a humanidade vive em constante medo dos Titãs, criaturas gigantescas que devoram humanos. Eren Yeager, um jovem que perdeu sua mãe para um Titã, jura exterminar todas essas criaturas. Junto com seus amigos, Mikasa Ackerman e Armin Arlert, ele se alista no exército para realizar sua vingança e descobrir a verdade sobre o mundo além das muralhas.",
+        "genero": "Ação Aventura Fantasia Terror",
+        "link": "https://www.crunchyroll.com/series/GR751KNZY/attack-on-titan",
+        "tags": "Titãs Eren Yeager Mikasa Ackerman Armin Arlert Levi Ackerman Muralhas"
+    },
+    {
+        "titulo": "Hunter x Hunter",
+        "sinopse": "Gon Freecss embarca em uma jornada para encontrar seu pai, um renomado Hunter. Para isso, ele precisa passar no Exame Hunter, um teste extremamente difícil que só os mais fortes e habilidosos conseguem superar. Ao longo de sua aventura, Gon faz novos amigos e enfrenta perigosos inimigos, descobrindo um mundo cheio de mistérios e criaturas sobrenaturais.",
+        "genero": "Aventura Fantasia Shonen",
+        "link": "https://www.crunchyroll.com/series/GY3VKX1MR/hunter-x-hunter",
+        "tags": "Gon Freecss Killua Zoldyck Hisoka Nen Hunter Exame Hunter"
+    }
+]
